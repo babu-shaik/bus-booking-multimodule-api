e529f67b518ddaf3cdbaa88bda0aa105543593cb
Bademia Babu <bademia.shaik@itcinfotech.com>
2020-05-15 01:28:40 +0530

    service code added

--- bus-admin-service/src/main/java/com/bus/admin/service/BusAdminService.java
+++ bus-admin-service/src/main/java/com/bus/admin/service/BusAdminService.java
@@ -1,8 +1,15 @@
 package com.bus.admin.service;
 
+import com.bus.admin.model.BusRoutes;
 import com.bus.admin.repository.BusAdminRepository;
 
 public class BusAdminService {
 
 	private BusAdminRepository repository;
+	
+	public BusRoutes addRoute() {
+		
+		
+		return null;
+	}
 }
