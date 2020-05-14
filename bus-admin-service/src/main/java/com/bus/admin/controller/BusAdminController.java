6adfddc2100727d6fcd5fdc646d04d36456d39dc
babu-shaik <babu20.c2s@gmil.com>
2020-05-15 01:08:05 +0530

    'fromhp'

--- bus-admin-service/src/main/java/com/bus/admin/controller/BusAdminController.java
+++ bus-admin-service/src/main/java/com/bus/admin/controller/BusAdminController.java
@@ -5,10 +5,7 @@ import java.util.List;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-<<<<<<< HEAD
 import org.springframework.web.bind.annotation.RequestParam;
-=======
->>>>>>> 7a3f1218c34d3eca49fc4ebe65aeca6ab14e2403
 import org.springframework.web.bind.annotation.RestController;
 
 import com.bus.admin.model.BusRoutes;
@@ -19,11 +16,9 @@ public class BusAdminController {
 	
 	@RequestMapping("/saveRoute")
 	public ResponseEntity<BusRoutes> saveOrUpdateRoutes(@RequestBody BusRoutes routes){
-<<<<<<< HEAD
-		
-=======
+
 		System.out.println("insave");
->>>>>>> 7a3f1218c34d3eca49fc4ebe65aeca6ab14e2403
+
 		
 		return null;
 		
@@ -35,14 +30,13 @@ public class BusAdminController {
 		
 		return null;
 	}
-<<<<<<< HEAD
+
 	
 	@RequestMapping("/findRoute/{name}")
 	public String findByRoute(@RequestParam("name") String name) {
 		System.out.println("infindbyroute");
+		System.out.println("chnges from hp");
 		return null;
 	}
 	
-=======
->>>>>>> 7a3f1218c34d3eca49fc4ebe65aeca6ab14e2403
 }
